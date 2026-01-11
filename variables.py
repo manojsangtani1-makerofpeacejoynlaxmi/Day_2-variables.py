edc01e51eb2bcd231e63e0668259e5436861dc95
manojsangtani1-makerofpeacejoynlaxmi <manojsangtani1@gmail.com>
2026-01-10 20:24:17 -0800

    add variables.py for day 2

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Manoj"
+last_name = "Sangtani"
+
+full_name = first_name + " " + last_name
+
+country = "United States of America"
+city = "Villa Park"
+
+age = 44
+year = 2026
+
+is_married = True
+is_true = True
+is_light_on = True
+
+x, y, z = 1, 2, 3
